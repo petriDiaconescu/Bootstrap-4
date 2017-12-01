71984090ae887515b2a4e3f6aa015c30e8a7bf46
petrero <petrero@gmail.com>
2017-12-01 18:05:59 +0200

    Add a responsive image to About Us page with img-fluid bootstrap class

--- about.php
+++ about.php
@@ -3,6 +3,13 @@
   include 'templates/navigation.php';
   $title = 'About Us';
 ?>
-<div class="container"></div>
+<div class="container">
+  <div class="row">
+    <div class="col-12 hero">
+      <img src="img/about_us.jpg" alt="" class="img-fluid"/>
+      <h2 class="text-uppercase">About Us</h2>
+    </div>
+  </div>
+</div>
 
 <?php include 'templates/footer.php' ?>
