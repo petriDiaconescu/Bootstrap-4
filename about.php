467a5992b8c0ff225852160f0c1728e8ed738ab7
petrero <petrero@gmail.com>
2017-12-01 19:02:30 +0200

    Add two paragraphs to our about us page

--- about.php
+++ about.php
@@ -15,7 +15,23 @@
 <div class="container">
   <div class="row">
     <main class="col-lg-8 main-content">
-      Main Content
+      <p>Duis lacus dui, imperdiet at iaculis ut, semper non 
+        lectus. Integer velit massa, pellentesque quis volutpat 
+        non, mattis eget mi. Proin massa tortor, laoreet sit 
+        amet dapibus sit amet, sodales quis metus. Nulla massa 
+        arcu, luctus vitae libero sed, vestibulum mollis est. 
+        Duis nec vehicula ligula. Pellentesque rhoncus 
+        scelerisque luctus. Morbi id interdum est, quis tempor 
+        lectus. Ut faucibus, leo et feugiat fermentum, dolor urna 
+        convallis dolor, vitae fringilla sem odio ut erat. Lorem 
+        ipsum dolor sit amet, consectetur adipiscing elit. 
+      </p>
+      <p>Class aptent taciti sociosqu ad litora torquent per conubia 
+        nostra, per inceptos himenaeos. Duis libero est, 
+        fringilla posuere ornare eget, bibendum eget metus. 
+        Interdum et malesuada fames ac ante ipsum primis in 
+        faucibus.
+      </p>
     </main>
     
     <aside class="col-lg-4">
