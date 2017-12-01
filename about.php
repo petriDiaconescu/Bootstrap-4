7a0fd6c0ff8751e7c4b05ff8eddb6fd94a08980c
petrero <petrero@gmail.com>
2017-12-01 19:41:47 +0200

    Copy code from branch 3.2

---
+++ about.php
@@ -0,0 +1,46 @@
+<?php
+  include 'templates/header.php';
+  include 'templates/navigation.php';
+  $title = 'About Us';
+?>
+<div class="container pt-4">
+  <div class="row no-gutters">
+    <div class="col-12 hero">
+      <img src="img/about_us.jpg" alt="" class="img-fluid"/>
+      <h2 class="text-uppercase d-none d-md-block">About Us</h2>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <main class="col-lg-8 main-content">
+      <h2 class="d-block d-md-none text-center text-uppercase">About Us</h2>
+      <p>Duis lacus dui, imperdiet at iaculis ut, semper non 
+        lectus. Integer velit massa, pellentesque quis volutpat 
+        non, mattis eget mi. Proin massa tortor, laoreet sit 
+        amet dapibus sit amet, sodales quis metus. Nulla massa 
+        arcu, luctus vitae libero sed, vestibulum mollis est. 
+        Duis nec vehicula ligula. Pellentesque rhoncus 
+        scelerisque luctus. Morbi id interdum est, quis tempor 
+        lectus. Ut faucibus, leo et feugiat fermentum, dolor urna 
+        convallis dolor, vitae fringilla sem odio ut erat. Lorem 
+        ipsum dolor sit amet, consectetur adipiscing elit. 
+      </p>
+      <p>Class aptent taciti sociosqu ad litora torquent per conubia 
+        nostra, per inceptos himenaeos. Duis libero est, 
+        fringilla posuere ornare eget, bibendum eget metus. 
+        Interdum et malesuada fames ac ante ipsum primis in 
+        faucibus.
+      </p>
+    </main>
+    
+    <aside class="col-lg-4">
+      <div class="sidebar">
+        sidebar
+      </div>
+    </aside>
+  </div>
+</div>
+
+<?php include 'templates/footer.php' ?>
