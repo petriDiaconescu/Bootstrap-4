aa35ef550f4bd1304b3b1688905c3a08d5c9d6a7
petrero <petrero@gmail.com>
2017-12-01 18:25:02 +0200

    Copy code from branch 3.1

---
+++ about.php
@@ -0,0 +1,29 @@
+<?php
+  include 'templates/header.php';
+  include 'templates/navigation.php';
+  $title = 'About Us';
+?>
+<div class="container pt-4">
+  <div class="row">
+    <div class="col-12 hero">
+      <img src="img/about_us.jpg" alt="" class="img-fluid"/>
+      <h2 class="text-uppercase">About Us</h2>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <main class="col-lg-8 main-content">
+      Main Content
+    </main>
+    
+    <aside class="col-lg-4">
+      <div class="sidebar">
+        sidebar
+      </div>
+    </aside>
+  </div>
+</div>
+
+<?php include 'templates/footer.php' ?>
