fe6e5ae4b4b7fbfd2b560df75f6af08e1d4fe6dd
petrero <petrero@gmail.com>
2017-12-02 12:53:06 +0200

    Copy code from branch 3.4

---
+++ templates/business_hours.php
@@ -0,0 +1,51 @@
+<h2 class="text-center text-uppercase">Business Hours</h2>
+<p class="text-center mt-5">Morbi id interdum est, quis tempor lectus. Ut faucibus, 
+  leo et feugiat fermentum, dolor urna convallis dolor, vitae 
+  fringilla sem odio ut erat.
+</p>
+<table class="table table-hover text-center mt-5">
+  <thead class="table-danger">
+    <tr>
+      <th class="text-center">Day</th>
+      <th class="text-center">From</th>
+      <th class="text-center">To</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Monday</td>
+      <td>09:00</td>
+      <td>19:00</td>
+    </tr>
+    <tr>
+      <td>Tuesday</td>
+      <td>09:00</td>
+      <td>19:00</td>
+    </tr>
+    <tr>
+      <td>Wednesday</td>
+      <td>09:00</td>
+      <td>19:00</td>
+    </tr>
+    <tr>
+      <td>Thursday</td>
+      <td>09:00</td>
+      <td>19:00</td>
+    </tr>
+    <tr>
+      <td>Friday</td>
+      <td>09:00</td>
+      <td>19:00</td>
+    </tr>
+    <tr>
+      <td>Saturday</td>
+      <td>10:00</td>
+      <td>12:00</td>
+    </tr>
+    <tr>
+      <td>Sunday</td>
+      <td>Closed</td>
+      <td>Closed</td>
+    </tr>
+  </tbody>
+</table>
