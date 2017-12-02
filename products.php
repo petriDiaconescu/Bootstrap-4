4fcee82a5158965debb8a4ac8b0278eac94abe7d
petrero <petrero@gmail.com>
2017-12-02 17:20:08 +0200

    Start products page

---
+++ products.php
@@ -0,0 +1,26 @@
+<?php
+  include 'templates/header.php';
+  include 'templates/navigation.php';
+  $title = 'Products / Store';
+?>
+<div class="container pt-4">
+  <div class="row no-gutters">
+    <div class="col-12 hero">
+      <img src="img/store.jpg" alt="" class="img-fluid"/>
+      <h2 class="text-uppercase d-none d-md-block">Products</h2>
+    </div>
+  </div>
+</div>
+
+<div class="container py-4">
+  <div class="row">
+    <main class="col-12 primary-content"> 
+      <h2 class="d-block d-md-none text-uppercase text-center">Products</h2>
+       
+    </main>  
+  </div>
+</div>
+
+<?php 
+  include 'templates/footer.php' 
+?>
--- templates/navigation.php
+++ templates/navigation.php
@@ -12,7 +12,7 @@
             <a href="services.php" class="nav-link">Services</a>
           </li>
           <li class="nav-item">
-            <a href="" class="nav-link">Products</a>
+            <a href="products.php" class="nav-link">Products</a>
           </li>
           <li class="nav-item">
             <a href="" class="nav-link">Contact Us</a>
