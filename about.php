c7bb0ac4a51f1621f574b76b9bf203692b04d70f
petrero <petrero@gmail.com>
2017-12-02 11:52:48 +0200

    Style aside section of about us page

--- about.php
+++ about.php
@@ -35,8 +35,8 @@
       </p>
     </main>
     
-    <aside class="col-lg-4">
-      <div class="sidebar">
+    <aside class="col-lg-4 pt-4 pt-lg-0">
+      <div class="sidebar business-hours p-3">
         <?php include 'templates/business_hours.php' ?>
       </div>
     </aside>
