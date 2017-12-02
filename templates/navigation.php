a764437e5f5316cceba28608b008a2a68cd46b2a
petrero <petrero@gmail.com>
2017-12-02 16:52:19 +0200

    Copy code from branch 4.4

---
+++ templates/navigation.php
@@ -0,0 +1,23 @@
+  <div class="navigation mt-4 py-1">
+    <nav class="main-nav py-1">
+      <div class="container">
+        <ul class="nav nav-justified flex-column flex-sm-row">
+          <li class="nav-item">
+            <a href="index.php" class="nav-link">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="about.php" class="nav-link">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a href="services.php" class="nav-link">Services</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">Products</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
