a5464b7e2077997d8edd349d8984cb8ca67cda9b
petrero <petrero@gmail.com>
2017-12-03 13:11:26 +0200

    Retrieve the informations being displayed on product page from database.

--- product.php
+++ product.php
@@ -1,7 +1,7 @@
 <?php
   include 'templates/header.php';
   include 'templates/navigation.php';
-  $title = 'Services';
+  
   
   $id = $_GET['product'];
   if(filter_var($id, FILTER_VALIDATE_INT) === FALSE){
@@ -10,7 +10,7 @@
   
   try{
     require_once 'inc/db.php';
-    $sql = 'SELECT `name`, `image_full`, `price`, `short_description`, `description` FROM `products` LIMIT 1';
+    $sql = 'SELECT `name`, `image_full`, `price`, `short_description`, `description` FROM `products` WHERE id = ' .$id . ' LIMIT 1';
     $results = $db->query($sql);
   } catch (Exception $e) {
     $error = $e->getMessage();
@@ -24,12 +24,13 @@
   } else {
     while($product = $results->fetch_assoc()){
     
+    $title = $product['name'];  
 ?>
 <div class="container pt-4">
   <div class="row no-gutters">
     <div class="col-12 hero">
-      <img src="img/product_large_01.jpg" alt="" class="img-fluid"/>
-      <h2 class="text-uppercase d-none d-md-block">Name of Product</h2>
+      <img src="img/<?php echo $product['image_full']; ?>" alt="" class="img-fluid"/>
+      <h2 class="text-uppercase d-none d-md-block"><?php echo $product['name']; ?></h2>
     </div>
   </div>
 </div>
@@ -37,43 +38,20 @@
 <div class="container py-4">
   <div class="row">
     <main class="col-lg-8 primary-content"> 
-      <h2 class="d-block d-md-none text-uppercase text-center">Name of Product</h2>
-      
-      <p>
-        Mauris id felis interdum, consectetur elit ac, imperdiet nisl. Cras laoreet 
-        purus vestibulum, rhoncus sapien non, pellentesque ligula. Sed at dui et eros 
-        molestie sollicitudin. Sed efficitur, est rhoncus luctus auctor, augue enim 
-        fermentum nibh, sit amet tempus orci neque ut leo. Integer vitae vestibulum urna. 
-        Nunc vulputate, libero non tempus cursus, sem ligula dignissim augue, quis 
-        molestie urna eros sed enim. Nam feugiat condimentum nunc, id aliquam tortor 
-        rutrum eget. Duis eu lobortis ipsum. Donec ullamcorper, turpis in posuere 
-        lobortis, lorem nibh bibendum magna, ac molestie nisi lacus at est. Quisque 
-        ultricies scelerisque metus, ut commodo mauris maximus non. Mauris consequat 
-        feugiat elementum. Ut dictum tristique metus vel aliquam. Suspendisse placerat 
-        vel mi vitae auctor.
-      </p>
-      
-      <p>
-        Maecenas blandit in nisl id dictum. Donec efficitur 
-        eu nulla vel elementum. Phasellus tincidunt mollis 
-        sem ac sollicitudin. Duis finibus diam sit amet 
-        vulputate sagittis. Etiam sed viverra magna, sed 
-        laoreet ligula. Curabitur vitae nulla sit amet eros 
-        ornare semper id sit amet odio. Mauris rhoncus porta 
-        sem a ornare. In feugiat est arcu, et feugiat est 
-        lacinia quis. Cras quis laoreet lacus.
+      <h2 class="d-block d-md-none text-uppercase text-center"><?php echo $product['name']; ?></h2>
+      <p> 
+        <?php echo $product['description']; ?>
       </p>
     </main>
     
     <aside class="col-lg-4 pt-4 pt-lg-0" id="product_description">
       <h3 class="text-uppercase text-center mt-5">Description</h3>
       <p class="text-center lead p-3">
-        In feugiat est arcu, et feugiat est 
-        lacinia quis. Cras quis laoreet lacus.
+        <?php echo $product['short_description']; ?>
       </p>
       
       <h3 class="text-uppercase text-center mt-2">Price</h3>
-      <p class="text-center lead p-3 display-4">$25</p>
+      <p class="text-center lead p-3 display-4">$<?php echo $product['price']; ?></p>
     </aside>
   </div>
 </div>
