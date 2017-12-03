15cc3ee52a3a520ec3bee7117221cf432b8574c4
petrero <petrero@gmail.com>
2017-12-03 14:46:14 +0200

    Case when name length is greater than three.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
   $('#contact_form').on('submit', function(event){
     var name = $('#name').val();
-    if (name.length < 3){
-      var nameField = $('#name');
+    var nameField = $('#name');
       var nameDiv = nameField.parent();
-      
+    if (name.length < 3){
       nameField.addClass('form-control-danger');
       nameDiv.addClass('has-danger')
         .append('<small class="form-text text-muted">Not a valid name</small>');
       
       nameDiv.find('label').addClass('col-form-label');
-    } 
+    } else {
+      nameField.addClass('form-control-success');
+      nameDiv.addClass('has-success')
+        .append('<small class="form-text text-muted">Valid!</small>');
+      nameDiv.find('label').addClass('col-form-label');
+    }
     
     event.preventDefault();
   });
