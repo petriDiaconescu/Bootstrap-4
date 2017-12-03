6eea09234d744ddd258130ba4e09a52642a1fc7d
petrero <petrero@gmail.com>
2017-12-03 12:18:17 +0200

    Copy code from branch 7.4

---
+++ products.php
@@ -0,0 +1,145 @@
+<?php
+  include 'templates/header.php';
+  include 'templates/navigation.php';
+  $title = 'Products / Store';
+?>
+<div class="container pt-4">
+  <div class="row no-gutters">
+    <div class="col-12 hero">
+      <img src="img/store.jpg" alt="" class="img-fluid"/>
+      <h2 class="text-uppercase d-none d-md-block">Products</h2>
+    </div>
+  </div>
+</div>
+
+<div class="container py-4">
+  <div class="row">
+    <main class="col-12 primary-content"> 
+      <h2 class="d-block d-md-none text-uppercase text-center">Products</h2>
+      
+      <div class="row products">
+        <div class="col-6 col-md-3 mb-5 mb-md-0">
+          <div class="card">
+            <a href="">
+              <img src="img/product_thumb_01.jpg" alt="" class="card-img-top img-fluid" />
+              <div class="card-block">
+                <h3 class="card-title text-center text-uppercase mb-0">Product 1</h3>
+                <p class="card-text text-uppercase">Vivamus auctor, velit ut tincidunt 
+                  molestie, elit metus iaculis sapien
+                </p>
+                <p class="price text-center mb-0">$ 25</p>
+              </div>
+            </a>
+          </div>
+        </div><!-- col-6 col-md-3   -->
+        <div class="col-6 col-md-3">
+          <div class="card">
+            <a href="">
+              <img src="img/product_thumb_02.jpg" alt="" class="card-img-top img-fluid" />
+              <div class="card-block">
+                <h3 class="card-title text-center text-uppercase mb-0">Product 2</h3>
+                <p class="card-text text-uppercase">Vivamus auctor, velit ut tincidunt 
+                  molestie, elit metus iaculis sapien
+                </p>
+                <p class="price text-center mb-0">$ 25</p>
+              </div>
+            </a>
+          </div>
+        </div><!-- col-6 col-md-3   -->
+        <div class="col-6 col-md-3">
+          <div class="card">
+            <a href="">
+              <img src="img/product_thumb_03.jpg" alt="" class="card-img-top img-fluid" />
+              <div class="card-block">
+                <h3 class="card-title text-center text-uppercase mb-0">Product 3</h3>
+                <p class="card-text text-uppercase">Vivamus auctor, velit ut tincidunt 
+                  molestie, elit metus iaculis sapien
+                </p>
+                <p class="price text-center mb-0">$ 25</p>
+              </div>
+            </a>
+          </div>
+        </div><!-- col-6 col-md-3   -->
+        <div class="col-6 col-md-3">
+          <div class="card">
+            <a href="">
+              <img src="img/product_thumb_04.jpg" alt="" class="card-img-top img-fluid" />
+              <div class="card-block">
+                <h3 class="card-title text-center text-uppercase mb-0">Product 4</h3>
+                <p class="card-text text-uppercase">Vivamus auctor, velit ut tincidunt 
+                  molestie, elit metus iaculis sapien
+                </p>
+                <p class="price text-center mb-0">$ 25</p>
+              </div>
+            </a>
+          </div>
+        </div><!-- col-6 col-md-3   -->
+      
+        <div class="w-100 py-2"></div>
+        
+        <div class="col-6 col-md-3 mb-5 mb-md-0">
+          <div class="card">
+            <a href="">
+              <img src="img/product_thumb_05.jpg" alt="" class="card-img-top img-fluid" />
+              <div class="card-block">
+                <h3 class="card-title text-center text-uppercase mb-0">Product 5</h3>
+                <p class="card-text text-uppercase">Vivamus auctor, velit ut tincidunt 
+                  molestie, elit metus iaculis sapien
+                </p>
+                <p class="price text-center mb-0">$ 25</p>
+              </div>
+            </a>
+          </div>
+        </div><!-- col-6 col-md-3   -->
+        <div class="col-6 col-md-3">
+          <div class="card">
+            <a href="">
+              <img src="img/product_thumb_06.jpg" alt="" class="card-img-top img-fluid" />
+              <div class="card-block">
+                <h3 class="card-title text-center text-uppercase mb-0">Product 6</h3>
+                <p class="card-text text-uppercase">Vivamus auctor, velit ut tincidunt 
+                  molestie, elit metus iaculis sapien
+                </p>
+                <p class="price text-center mb-0">$ 25</p>
+              </div>
+            </a>
+          </div>
+        </div><!-- col-6 col-md-3   -->
+        <div class="col-6 col-md-3">
+          <div class="card">
+            <a href="">
+              <img src="img/product_thumb_07.jpg" alt="" class="card-img-top img-fluid" />
+              <div class="card-block">
+                <h3 class="card-title text-center text-uppercase mb-0">Product 7</h3>
+                <p class="card-text text-uppercase">Vivamus auctor, velit ut tincidunt 
+                  molestie, elit metus iaculis sapien
+                </p>
+                <p class="price text-center mb-0">$ 25</p>
+              </div>
+            </a>
+          </div>
+        </div><!-- col-6 col-md-3   -->
+        <div class="col-6 col-md-3">
+          <div class="card">
+            <a href="">
+              <img src="img/product_thumb_08.jpg" alt="" class="card-img-top img-fluid" />
+              <div class="card-block">
+                <h3 class="card-title text-center text-uppercase mb-0">Product 8</h3>
+                <p class="card-text text-uppercase">Vivamus auctor, velit ut tincidunt 
+                  molestie, elit metus iaculis sapien
+                </p>
+                <p class="price text-center mb-0">$ 25</p>
+              </div>
+            </a>
+          </div>
+        </div><!-- col-6 col-md-3   -->
+      </div><!--row products -->
+    </main>  
+  </div>
+</div>
+
+
+
+<?php 
+  include 'templates/footer.php' 
+?>
