0e8626f00b721358b18eba6d46ee52385129cd73
petrero <petrero@gmail.com>
2017-12-03 14:59:55 +0200

    Refactor validation jQuery code

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,14 @@ $(document).ready(function(){
   $('#contact_form').on('submit', function(event){
     var name = $('#name').val();
     if (name.length < 4){
-      $('#name').parent().addClass('has-danger from-control-danger');
-      $('#name').parent().find('label').addClass('col-form-label');
-      $('#name').parent().append('<small class="form-text text-muted">Not a valid name</small>');
+      var nameField = $('#name');
+      var nameDiv = nameField.parent();
+      
+      nameField.addClass('form-control-danger');
+      nameDiv.addClass('has-danger')
+        .append('<small class="form-text text-muted">Not a valid name</small>');
+      
+      nameDiv.find('label').addClass('col-form-label');
     }
     event.preventDefault();
   });
