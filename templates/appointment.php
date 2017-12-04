d0ec7d1e84f345592f218dd80a6736400ccb086c
petrero <petrero@gmail.com>
2017-12-04 16:30:36 +0200

    Copy code from branch 9.1

---
+++ templates/appointment.php
@@ -0,0 +1,13 @@
+<div class="appointment container-fluid py-5">
+  <div class="row">
+    <div class="col-12 col-md-8 offset-md-2 col-lg-5 offset-lg-3 py-3 text-center">
+      <h3 class="text-uppercase">make an appointment</h3>
+      <p>Vivamus sit amet tortor tristique, maximus risus et, gravida velit. 
+        Sed maximus semper leo, ac maximus arcu placerat id. Praesent non 
+        dapibus libero.
+      </p>
+      <a href="" class="btn btn-primary btn-lg mt-3 text-uppercase">read more</a>
+    </div><!-- col-12 -->
+  </div><!-- row -->
+</div><!-- appointment -->
+
---
+++ templates/navigation.php
@@ -0,0 +1,33 @@
+  <div class="navigation mt-4 py-1">
+    <nav class="main-nav py-1 navbar navbar-toggleable-md navbar-light bg-faded">
+      <button type="button" 
+        class="navbar-toggler navbar-toggler-right"
+        data-toggle="collapse"
+        data-target="#main-navigation"
+        aria-expanded="false"
+        aria-label="Toggle Navigation">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+      <a href="#" class="navbar-brand">Carolina Spa</a>
+      
+      <div class="collapse navbar-collapse" id="main-navigation">
+        <ul class="nav nav-justified flex-column flex-sm-row">
+          <li class="nav-item">
+            <a href="index.php" class="nav-link">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="about.php" class="nav-link">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a href="services.php" class="nav-link">Services</a>
+          </li>
+          <li class="nav-item">
+            <a href="products.php" class="nav-link">Products</a>
+          </li>
+          <li class="nav-item">
+            <a href="contact.php" class="nav-link">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
