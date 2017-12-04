249ca98bec2ce884c68e627e39b60e370e96873d
petrero <petrero@gmail.com>
2017-12-04 20:46:24 +0200

    Point the link resources to the new version of bootstrap(4.0.0-beta2). As you can see our site are missing something now

--- templates/footer.php
+++ templates/footer.php
@@ -60,8 +60,8 @@
     </div><!-- container -->
   </footer>
   <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
-    <script src="js/tether.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
     <script src="js/scripts.js"></script>
   </body>
 </html>
