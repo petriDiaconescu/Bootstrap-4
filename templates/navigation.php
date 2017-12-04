84899e739af8b01d46d7334a44f74562f5be7449
petrero <petrero@gmail.com>
2017-12-04 17:02:51 +0200

    Enclose collapsable navbar in container div

--- templates/navigation.php
+++ templates/navigation.php
@@ -9,25 +9,26 @@
           <span class="navbar-toggler-icon"></span>
       </button>
       <a href="#" class="navbar-brand d-md-none">Carolina Spa</a>
-      
-      <div class="collapse navbar-collapse" id="main-navigation">
-        <ul class="nav nav-justified flex-column flex-sm-row">
-          <li class="nav-item">
-            <a href="index.php" class="nav-link">Home</a>
-          </li>
-          <li class="nav-item">
-            <a href="about.php" class="nav-link">About Us</a>
-          </li>
-          <li class="nav-item">
-            <a href="services.php" class="nav-link">Services</a>
-          </li>
-          <li class="nav-item">
-            <a href="products.php" class="nav-link">Products</a>
-          </li>
-          <li class="nav-item">
-            <a href="contact.php" class="nav-link">Contact Us</a>
-          </li>
-        </ul>
-      </div>
+      <div class="container">
+        <div class="collapse navbar-collapse" id="main-navigation">
+          <ul class="nav nav-justified flex-column flex-sm-row">
+            <li class="nav-item">
+              <a href="index.php" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+              <a href="about.php" class="nav-link">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a href="services.php" class="nav-link">Services</a>
+            </li>
+            <li class="nav-item">
+              <a href="products.php" class="nav-link">Products</a>
+            </li>
+            <li class="nav-item">
+              <a href="contact.php" class="nav-link">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      </div>  
     </nav>
   </div>
