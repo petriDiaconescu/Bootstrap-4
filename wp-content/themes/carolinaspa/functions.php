bc9f6911df3057ac194b6c476a03981f475a7960
petrero <petrero@gmail.com>
2017-12-05 13:27:20 +0200

    Register social menu in our wordpress setup.

--- wp-content/themes/carolinaspa/functions.php
+++ wp-content/themes/carolinaspa/functions.php
@@ -1,5 +1,5 @@
 <?php
-
+/** add javascripts and stylesheets */
 function carolinaspa_styles(){
   wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.0.0');
   wp_enqueue_style('fontawesome', get_template_directory_uri() . '/css/font-awesome.min.css', array(), '4.7.0');
@@ -9,3 +9,11 @@ function carolinaspa_styles(){
 
 add_action('wp_enqueue_scripts', 'carolinaspa_styles');
 
+//Setup theme
+function carolinaspa_setup(){
+  register_nav_menus(array(
+    'social_menu' =>  esc_html__('Social', 'carolinaspa')    
+  ));
+}
+
+add_action('after_setup_theme', 'carolinaspa_setup');
