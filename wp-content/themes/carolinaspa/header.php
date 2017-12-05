db55477aa061bacd14bfe21a6b5bf2f9a47ad9d4
petrero <petrero@gmail.com>
2017-12-05 14:09:26 +0200

    Copy code from navigation template in branch 9.3

--- wp-content/themes/carolinaspa/header.php
+++ wp-content/themes/carolinaspa/header.php
@@ -32,6 +32,41 @@
       
       
     </header>
+    
+    <div class="navigation mt-4 py-1">
+      <nav class="main-nav py-1 navbar navbar-toggleable-sm navbar-light bg-faded">
+        <button type="button" 
+          class="navbar-toggler navbar-toggler-right"
+          data-toggle="collapse"
+          data-target="#main-navigation"
+          aria-expanded="false"
+          aria-label="Toggle Navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a href="#" class="navbar-brand d-md-none">Carolina Spa</a>
+        <div class="container">
+          <div class="collapse navbar-collapse" id="main-navigation">
+            <ul class="nav nav-justified flex-column flex-sm-row">
+              <li class="nav-item">
+                <a href="index.php" class="nav-link">Home</a>
+              </li>
+              <li class="nav-item">
+                <a href="about.php" class="nav-link">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a href="services.php" class="nav-link">Services</a>
+              </li>
+              <li class="nav-item">
+                <a href="products.php" class="nav-link">Products</a>
+              </li>
+              <li class="nav-item">
+                <a href="contact.php" class="nav-link">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>  
+      </nav>
+    </div>
 
 
 
