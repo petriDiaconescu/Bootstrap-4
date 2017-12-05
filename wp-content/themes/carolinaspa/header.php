a0900cda56d1931b6049314bf9690ff0c4ce31ad
petrero <petrero@gmail.com>
2017-12-05 13:11:26 +0200

    Copy code from bra branch 12.1

---
+++ wp-content/themes/carolinaspa/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Carolina Spa <?php echo $title; ?></title>
+    <?php wp_head(); ?>
+  </head>
+  <body>
+    <header class="site-header container">
+      <div class="row justify-content-between">
+        <div class="col-8 offset-2 col-lg-4 offset-lg-0">
+          <a href="index.php">
+            <img src="<?php echo get_template_directory_uri() ?>/img/logo.png" alt="" class="img-fluid mx-auto d-block" />
+          </a>
+        </div> 
+        <div class="col-12 col-lg-4">
+          <nav class="socials text-center text-md-right pt-3">
+            <ul>
+              <li>
+                <a href="http://facebook.com">
+                  <span class="sr-only">Facebook</span>
+                </a>
+              </li>
+              <li>
+                <a href="http://twitter.com">
+                  <span class="sr-only">Twitter</span>
+                </a>
+              </li>
+              <li>
+                <a href="http://instagram.com">
+                  <span class="sr-only">Instagram</span>
+                </a>
+              </li>
+              <li>
+                <a href="http://pinterest.com">
+                  <span class="sr-only">Pinterest</span>
+                </a>
+              </li>
+              <li>
+                <a href="http://youtube.com">
+                  <span class="sr-only">Youtube</span>
+                </a>
+              </li>
+            </ul>
+            
+          </nav>
+        </div>
+      </div><!-- justify-content-between -->
+    </header>
+
+
+
