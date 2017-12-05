b61886e0e5e73b8020e089918cf0c9189c973c42
petrero <petrero@gmail.com>
2017-12-05 17:12:39 +0200

    Copy code from branch 13.3

---
+++ wp-content/themes/carolinaspa/functions.php
@@ -0,0 +1,75 @@
+<?php
+/** add javascripts and stylesheets */
+function carolinaspa_styles(){
+  wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.0.0');
+  wp_enqueue_style('fontawesome', get_template_directory_uri() . '/css/font-awesome.min.css', array(), '4.7.0');
+  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Italianno|Lato:400,700,900|Raleway:400,700,900');
+  wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0');
+}
+
+add_action('wp_enqueue_scripts', 'carolinaspa_styles');
+
+//Setup theme
+function carolinaspa_setup(){
+  register_nav_menus(array(
+    'main_menu' =>  esc_html__('Main_Menu', 'carolinaspa'),   
+    'social_menu' =>  esc_html__('Social', 'carolinaspa')    
+  ));
+}
+
+add_action('after_setup_theme', 'carolinaspa_setup');
+
+//Add bootstrap nav-item class to the <li> of the Main menu
+function carolinaspa_custom_li_class($classes, $item, $args){
+  if($args->theme_location == 'main_menu'){
+    $classes[] = 'nav-item';
+  }
+  
+  return $classes;
+}
+
+add_filter('nav_menu_css_class', 'carolinaspa_custom_li_class', 1, 3);
+
+//Add bootstrap nav-link class to the <a> of the Main menu
+function carolinaspa_custom_anchor_class($atts, $item, $args){
+  if($args->theme_location == 'main_menu'){
+    $class = 'class';
+    $atts[$class] = 'nav-link';
+  }
+  
+  return $atts;
+}
+
+add_filter('nav_menu_link_attributes', 'carolinaspa_custom_anchor_class', 10, 3);
+
+//widgets
+function carolinaspa_widgets(){
+  register_sidebar(array(
+    'name'            => 'Footer Widget 1',
+    'id'              => 'footer_widget_1',
+    'before_widget'   => '<div id="%1$s">',
+    'after_widget'    => '</div>',
+    'before_title'    => '<h3 class="text-uppercase text-center pb-4">',
+    'after_title'     => '</h3>'  
+  ));
+  register_sidebar(array(
+    'name'            => 'Footer Widget 2',
+    'id'              => 'footer_widget_2',
+    'before_widget'   => '<div id="%1$s">',
+    'after_widget'    => '</div>',
+    'before_title'    => '<h3 class="text-uppercase text-center pb-4">',
+    'after_title'     => '</h3>'  
+  ));
+  
+  register_sidebar(array(
+    'name'            => 'Footer Widget 3',
+    'id'              => 'footer_widget_3',
+    'before_widget'   => '<div id="%1$s">',
+    'after_widget'    => '</div>',
+    'before_title'    => '<h3 class="text-uppercase text-center pb-4">',
+    'after_title'     => '</h3>'  
+  ));
+}
+
+
+add_action('widgets_init', 'carolinaspa_widgets');
---
+++ wp-content/themes/carolinaspa/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Carolina Spa <?php echo $title; ?></title>
+    <?php wp_head(); ?>
+  </head>
+  <body>
+    <header class="site-header container">
+      <div class="row justify-content-between">
+        <div class="col-8 offset-2 col-lg-4 offset-lg-0">
+          <a href="index.php">
+            <img src="<?php echo get_template_directory_uri() ?>/img/logo.png" alt="" class="img-fluid mx-auto d-block" />
+          </a>
+        </div> 
+        <div class="col-12 col-lg-4">
+          <?php 
+            $args = array(
+              'container'       => 'nav',
+              'container_class' => 'socials text-center text-md-right pt-3',
+              'link_before'     => '<span class="sr-only">',
+              'link_after'      => '</span>',
+              'theme_location'  => 'social_menu'  
+            );
+            wp_nav_menu($args);
+          ?>
+        </div>
+        
+      </div><!-- justify-content-between -->
+      
+      
+    </header>
+    
+    <div class="navigation mt-4 py-1">
+      <nav class="main-nav py-1 navbar navbar-toggleable-sm navbar-light bg-faded">
+        <button type="button" 
+          class="navbar-toggler navbar-toggler-right"
+          data-toggle="collapse"
+          data-target="#main-navigation"
+          aria-expanded="false"
+          aria-label="Toggle Navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a href="#" class="navbar-brand d-md-none">Carolina Spa</a>
+        <div class="container">
+          <?php
+            $args = array(
+              'menu_class'      => 'nav nav-justified flex-column flex-sm-row', //classes for the ul
+              'container_class' => 'collapse navbar-collapse', //classes for the div 
+              'container_id'    => 'main-navigation',  
+              'theme_location'  => 'main_menu'
+            );
+            wp_nav_menu($args);
+          ?>
+        </div>  
+      </nav>
+    </div>
+
+
+
