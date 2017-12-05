641a93131a6c677239dfa4cdf4571de8bfd402b4
petrero <petrero@gmail.com>
2017-12-05 21:30:47 +0200

    Copy code from branch 14.4

---
+++ wp-content/themes/carolinaspa/page.php
@@ -0,0 +1,37 @@
+<?php 
+  get_header(); 
+?> 
+
+<?php while(have_posts()): the_post() ?>
+  <div class="container pt-4">
+    <div class="row no-gutters">
+      <div class="col-12 hero">
+        <?php the_post_thumbnail('full', array('class' => 'img-fluid')) ?>
+        <h2 class="text-uppercase d-none d-md-block"><?php the_title() ?></h2>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <main class="col-lg-8 primary-content">
+        <h2 class="d-block d-md-none text-center text-uppercase"><?php the_title() ?></h2>
+        <?php the_content() ?>
+        <?php 
+          if(is_page('about-us')):
+            get_template_part('/templates/gallery');
+          endif;
+            
+        ?>
+      </main>
+
+      <aside class="col-lg-4 pt-4 pt-lg-0">
+        <div class="sidebar business-hours p-3">
+          <?php include 'templates/business_hours.php' ?>
+        </div>
+      </aside>
+    </div>
+  </div>
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
