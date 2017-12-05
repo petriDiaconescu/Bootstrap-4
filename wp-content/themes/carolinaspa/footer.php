f18d2072d6f5a24823972e444d0ce580f302470c
petrero <petrero@gmail.com>
2017-12-05 16:15:06 +0200

    Copy code from branch 13.1

---
+++ wp-content/themes/carolinaspa/footer.php
@@ -0,0 +1,49 @@
+ <footer class="site-footer pt-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <h3 class="text-uppercase text-center pb-4">About Us</h3>
+          <p class="text-justify">Nullam a massa a risus congue egestas. Morbi venenatis, 
+            ex at tincidunt cursus, lorem arcu iaculis mi, vel vehicula 
+            elit nisl eget sapien. Proin vel nisi at augue cursus hendrerit. 
+            Sed consectetur pulvinar arcu eu maximus
+          </p>
+        </div>
+        <div class="col-md-4 pb-4 pb-md-0">
+          <h3 class="text-uppercase text-center pb-4">Open hours</h3> 
+          <p class="text-center mb-0">Mon-Fri: 9 AM - 7 PM</p>
+          <p class="text-center mb-0">Saturday: 10 AM - 2 PM</p>
+          <p class="text-center mb-0">Sunday: Closed</p>
+        </div>
+        <div class="col-md-4 pb-4 pb-md-0">
+          <h3 class="text-uppercase text-center pb-4">Contact</h3> 
+          <p class="mb-0">66 East Sunnyslope Avenue</p>
+          <p>Lansdowne, PA 19050</p>
+          <div class="social-nav">
+            
+            <?php 
+              $args = array(
+                'container'       => 'nav',
+                'container_class' => 'socials pt-3',
+                'link_before'     => '<span class="sr-only">',
+                'link_after'      => '</span>',
+                'theme_location'  => 'social_menu'  
+              );
+              wp_nav_menu($args);
+            ?>
+          </div>
+          
+        </div>
+
+        <div class="w-100"></div>
+        <hr class="w-100" />
+        <p class="copyright text-center w-100">Carolina Spa & Salon 2017</p>
+      </div><!-- row -->
+    </div><!-- container -->
+  </footer>
+  <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
