12a243dddad971214e29d58b437122cf892986aa
petrero <petrero@gmail.com>
2017-12-05 12:14:29 +0200

    Enqueue bootstrap, font-awesome and styles stylesheets.

--- wp-content/themes/carolinaspa/functions.php
+++ wp-content/themes/carolinaspa/functions.php
@@ -1,7 +1,9 @@
 <?php
 
 function carolinaspa_styles(){
-  wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0');
+  wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.0.0');
+  wp_enqueue_style('fontawesome', get_template_directory_uri() . '/css/font-awesome.min.css', array(), '4.7.0');
+  wp_enqueue_style('style', get_template_directory_uri() . '/css/styles.css', array(), '1.0');
 }
 
 add_action('wp_enqueue_scripts', 'carolinaspa_styles');
