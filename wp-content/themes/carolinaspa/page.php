b5e6f163b63d59eb863b9ece749c0e411685a9cd
petrero <petrero@gmail.com>
2017-12-06 14:38:37 +0200

    Move the code responsible for gallery in another file and include it in page php file.

--- wp-content/themes/carolinaspa/page.php
+++ wp-content/themes/carolinaspa/page.php
@@ -18,75 +18,11 @@
         <h2 class="d-block d-md-none text-center text-uppercase"><?php the_title() ?></h2>
         <?php the_content() ?>
 
-        <div class="facilities-gallery">
-
-          <h3 class="text-center text-uppercase p-4">
-            <span class="text-lowercase">Checkout our</span>facilities
-          </h3>
-
-          <?php 
-            $gallery = get_field('gallery', $post, false);
-            if($gallery){
-              preg_match('/\[gallery.*ids=.(.*).\]/', $gallery, $ids);
-              $images_id = explode(',', $ids[1]);
-              
-              foreach($images_id as $id):?>
-                <a href="" data-target="#image_<?php echo $id?>" data-toggle="modal">
-                  <?php $image_url = wp_get_attachment_image_src($id, 'thumbnail') ?>
-                  <img src="<?php echo $image_url[0] ?>" alt="" class="rounded" />
-                </a>
-          
-                <div class="modal fade" id="image_<?php echo $id?>" tabindex="-1" 
-                  role="dialog" aria-labelledby="image_<?php echo $id?>" 
-                  aria-hidden="true">
-                  <div class="modal-dialog modal-lg" role="document">
-                    <div class="modal-content">
-                      <div class="modal-body">
-                        <?php $image_url = wp_get_attachment_image_src($id, 'full') ?>
-                        <img src="<?php echo $image_url[0] ?>" alt="" class="img-fluid" />
-                      </div>
-                    </div>
-                  </div><!-- modal-content -->
-                </div><!-- modal -->
-              <?php
-              endforeach;
-            }
-          ?>
-
-          <div class="modal fade" id="image_1" tabindex="-1" 
-            role="dialog" aria-labelledby="image_1" 
-            aria-hidden="true">
-            <div class="modal-dialog modal-lg" role="document">
-              <div class="modal-content">
-                <div class="modal-body">
-                  <img src="img/gallery_large_01.jpg" alt="" class="img-fluid"/>
-                </div>
-              </div>
-            </div><!-- modal-content -->
-          </div><!-- modal -->
-          <div class="modal fade" id="image_2" tabindex="-1" 
-            role="dialog" aria-labelledby="image_2" 
-            aria-hidden="true">
-            <div class="modal-dialog modal-lg" role="document">
-              <div class="modal-content">
-                <div class="modal-body">
-                  <img src="img/gallery_large_02.jpg" alt="" class="img-fluid"/>
-                </div>
-              </div>
-            </div><!-- modal-content -->
-          </div><!-- modal -->
-          <div class="modal fade" id="image_3" tabindex="-1" 
-            role="dialog" aria-labelledby="image_3" 
-            aria-hidden="true">
-            <div class="modal-dialog modal-lg" role="document">
-              <div class="modal-content">
-                <div class="modal-body">
-                  <img src="img/gallery_large_03.jpg" alt="" class="img-fluid"/>
-                </div>
-              </div>
-            </div><!-- modal-content -->
-          </div><!-- modal -->
-        </div><!-- facilities-gallery -->
+        <?php 
+          if(is_page('about-us')):
+            get_template_part('/templates/gallery');
+          endif; 
+        ?>
 
       </main>
 
---
+++ wp-content/themes/carolinaspa/templates/gallery.php
@@ -0,0 +1,34 @@
+<div class="facilities-gallery">
+
+  <h3 class="text-center text-uppercase p-4">
+    <span class="text-lowercase">Checkout our</span>facilities
+  </h3>
+
+  <?php 
+    $gallery = get_field('gallery', $post, false);
+    if($gallery){
+      preg_match('/\[gallery.*ids=.(.*).\]/', $gallery, $ids);
+      $images_id = explode(',', $ids[1]);
+      foreach($images_id as $id):?>
+        <a href="" data-target="#image_<?php echo $id?>" data-toggle="modal">
+          <?php $image_url = wp_get_attachment_image_src($id, 'thumbnail') ?>
+          <img src="<?php echo $image_url[0] ?>" alt="" class="rounded" />
+        </a>
+
+        <div class="modal fade" id="image_<?php echo $id?>" tabindex="-1" 
+          role="dialog" aria-labelledby="image_<?php echo $id?>" 
+          aria-hidden="true">
+          <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+              <div class="modal-body">
+                <?php $image_url = wp_get_attachment_image_src($id, 'full') ?>
+                <img src="<?php echo $image_url[0] ?>" alt="" class="img-fluid" />
+              </div>
+            </div>
+          </div><!-- modal-content -->
+        </div><!-- modal -->
+      <?php
+      endforeach;
+    }
+  ?>
+</div><!-- facilities-gallery -->
