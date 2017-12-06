fb6f602fd4cd4785a27b3acdfd71be11d0179f2f
petrero <petrero@gmail.com>
2017-12-06 14:30:21 +0200

    Make modal to work again

--- wp-content/themes/carolinaspa/page.php
+++ wp-content/themes/carolinaspa/page.php
@@ -33,9 +33,21 @@
               foreach($images_id as $id):?>
                 <a href="" data-target="#image_<?php echo $id?>" data-toggle="modal">
                   <?php $image_url = wp_get_attachment_image_src($id, 'thumbnail') ?>
-                  <pre><?php var_dump($image_url)?></pre>
                   <img src="<?php echo $image_url[0] ?>" alt="" class="rounded" />
                 </a>
+          
+                <div class="modal fade" id="image_<?php echo $id?>" tabindex="-1" 
+                  role="dialog" aria-labelledby="image_<?php echo $id?>" 
+                  aria-hidden="true">
+                  <div class="modal-dialog modal-lg" role="document">
+                    <div class="modal-content">
+                      <div class="modal-body">
+                        <?php $image_url = wp_get_attachment_image_src($id, 'full') ?>
+                        <img src="<?php echo $image_url[0] ?>" alt="" class="img-fluid" />
+                      </div>
+                    </div>
+                  </div><!-- modal-content -->
+                </div><!-- modal -->
               <?php
               endforeach;
             }
