ea2f2a7dd0984cb2e34c391b334d20e791fefeab
petrero <petrero@gmail.com>
2017-12-06 14:07:51 +0200

    Install Advanced Custom Fields: Table Field plugin. Use it with the wordpress interface to insert a table field in our about us template. Ad a text field in our wordpress about us template using wordpress interface. Create a sidebar php field where extract aside code. Use get_sidebar php function to get sidebar in our about us page.

--- wp-content/themes/carolinaspa/page.php
+++ wp-content/themes/carolinaspa/page.php
@@ -25,11 +25,7 @@
         ?>
       </main>
 
-      <aside class="col-lg-4 pt-4 pt-lg-0">
-        <div class="sidebar business-hours p-3">
-          <?php include 'templates/business_hours.php' ?>
-        </div>
-      </aside>
+      <?php get_sidebar() ?>
     </div>
   </div>
 <?php endwhile; ?>
---
+++ wp-content/themes/carolinaspa/sidebar.php
@@ -0,0 +1,5 @@
+<aside class="col-lg-4 pt-4 pt-lg-0">
+  <div class="sidebar business-hours p-3">
+    <?php include 'templates/business_hours.php' ?>
+  </div>
+</aside>
