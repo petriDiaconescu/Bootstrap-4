3e51672bc290352fb47ec7af6dcc57a340f9117b
petrero <petrero@gmail.com>
2017-12-06 13:41:53 +0200

    Copy code from branch 9.3 in sidebar php file.

--- wp-content/themes/carolinaspa/functions.php
+++ wp-content/themes/carolinaspa/functions.php
@@ -87,6 +87,24 @@ function carolinaspa_widgets(){
     'before_title'    => '<h3 class="text-uppercase text-center pb-4">',
     'after_title'     => '</h3>'  
   ));
+  
+  register_sidebar(array(
+    'name'            => 'Footer Widget 2',
+    'id'              => 'footer_widget_2',
+    'before_widget'   => '<div id="%1$s">',
+    'after_widget'    => '</div>',
+    'before_title'    => '<h3 class="text-uppercase text-center pb-4">',
+    'after_title'     => '</h3>'  
+  ));
+  
+  register_sidebar(array(
+    'name'            => 'Footer Widget 3',
+    'id'              => 'footer_widget_3',
+    'before_widget'   => '<div id="%1$s">',
+    'after_widget'    => '</div>',
+    'before_title'    => '<h3 class="text-uppercase text-center pb-4">',
+    'after_title'     => '</h3>'  
+  ));
 }
 
 
