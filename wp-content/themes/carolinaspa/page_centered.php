5cac18c343f28f9772811cadef418c89b0589376
petrero <petrero@gmail.com>
2017-12-07 15:53:44 +0200

    Create a new template called page-centered coped from page.php and in wordpress interface activate this template for Contact Us page.

---
+++ wp-content/themes/carolinaspa/page_centered.php
@@ -0,0 +1,36 @@
+<?php 
+  /** 
+   * Template Name: Full Width / Centered
+   */
+  get_header(); 
+?> 
+
+<?php while(have_posts()): the_post() ?>
+  <div class="container pt-4">
+    <div class="row no-gutters">
+      <div class="col-12 hero">
+        <?php the_post_thumbnail('full', array('class' => 'img-fluid')) ?>
+        <h2 class="text-uppercase d-none d-md-block"><?php the_title() ?></h2>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row  mt-lg-4">
+      <main class="col-lg-8 primary-content">
+        <h2 class="d-block d-md-none text-center text-uppercase"><?php the_title() ?></h2>
+        <?php the_content() ?>
+        <?php 
+          if(is_page('about-us')):
+            get_template_part('/templates/gallery');
+          endif;
+            
+        ?>
+      </main>
+
+      <?php get_sidebar() ?>
+    </div>
+  </div>
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
